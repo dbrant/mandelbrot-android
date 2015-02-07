f5671f8963f1753f1c308d73cc68d9984f9a3daf
dbrant <dbrant@wikimedia.org>
2015-02-07 13:09:34 -0500

    Updated to use support library with ActionBarActivity.

--- app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotActivity.java
+++ app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotActivity.java
@@ -1,5 +1,6 @@
 package com.dmitrybrant.android.mandelbrot;
 
+import java.lang.reflect.Field;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -10,11 +11,16 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
@@ -25,65 +31,38 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MandelbrotActivity extends Activity {
+public class MandelbrotActivity extends ActionBarActivity {
 	public static final String PREFS_NAME = "MandelbrotActivityPrefs";
-	
-	static{
+
+	static {
 		System.loadLibrary("mandelnative_jni");
 	}
-	
+
 	MandelbrotCanvas mandelbrotView;
 	FrameLayout topLayout;
 	View iterationsContainer;
 	public TextView txtInfo, txtIterations;
-	Button btnOptions, btnSave, btnReset;
-	Button btnJulia, btnAbout, btnColorScheme;
+	Button btnOptions;
+	Button btnColorScheme;
 	SeekBar seekBarIterations;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.main);
         topLayout = (FrameLayout)findViewById(R.id.topLayout);
-        
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+        forceOverflowMenuIcon(this);
+
         txtInfo = (TextView)findViewById(R.id.txtInfo);
         txtInfo.setVisibility(View.INVISIBLE);
-        
         txtIterations = (TextView)findViewById(R.id.txtIterations);
-        
-        btnJulia = (Button)findViewById(R.id.btnJulia);
-        btnJulia.getBackground().setAlpha(128);
-        btnJulia.setOnClickListener(new OnClickListener(){
-			public void onClick(View arg0) {
-	        	mandelbrotView.juliaMode = !mandelbrotView.juliaMode;
-	        	btnJulia.setText(mandelbrotView.juliaMode ? "Mandelbrot mode" : "Julia mode");
-	        	mandelbrotView.RenderMandelbrot();
-			}
-        });
-        
-        btnAbout = (Button)findViewById(R.id.btnAbout);
-        btnAbout.getBackground().setAlpha(128);
-        btnAbout.setVisibility(View.INVISIBLE);
-        btnAbout.setOnClickListener(new OnClickListener(){
-			public void onClick(View arg0) {
-			    LayoutInflater inflater = (LayoutInflater)MandelbrotActivity.this.getSystemService(LAYOUT_INFLATER_SERVICE);
-			    View layout = inflater.inflate(R.layout.about, null);
-			    TextView txtAbout = (TextView)layout.findViewById(R.id.txtAbout);
-			    txtAbout.setText(MandelbrotActivity.this.getString(R.string.str_about));
-				AlertDialog alertDialog = new AlertDialog.Builder(MandelbrotActivity.this).create();
-			    alertDialog.setTitle("About...");
-			    alertDialog.setView(layout);
-			    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-				    public void onClick(DialogInterface dialog, int which) {
-				    } });
-			    alertDialog.show();
-			}
-        });
-        
+
         btnColorScheme = (Button)findViewById(R.id.btnColorScheme);
         btnColorScheme.getBackground().setAlpha(128);
         btnColorScheme.setVisibility(View.INVISIBLE);
@@ -94,7 +73,7 @@ public class MandelbrotActivity extends Activity {
 	        	mandelbrotView.RenderMandelbrot();
 			}
         });
-        
+
         btnOptions = (Button)findViewById(R.id.btnOptions);
         btnOptions.getBackground().setAlpha(128);
         btnOptions.setOnClickListener(new OnClickListener(){
@@ -120,33 +99,7 @@ public class MandelbrotActivity extends Activity {
 	        	}
 			}
         });
-        
-        btnSave = (Button)findViewById(R.id.btnSave);
-        btnSave.getBackground().setAlpha(128);
-        btnSave.setVisibility(View.INVISIBLE);
-        btnSave.setOnClickListener(new OnClickListener(){
-			public void onClick(View arg0) {
-				try{
-					String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-					SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
-					path += "/" + f.format(new Date()) + ".png";
-					mandelbrotView.SavePicture(path);
-					Toast.makeText(MandelbrotActivity.this, "Picture saved as: " + path, Toast.LENGTH_SHORT).show();
-				}catch(Exception ex){
-					Toast.makeText(MandelbrotActivity.this, "Error saving file: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
-				}
-			}
-        });
-        
-        btnReset = (Button)findViewById(R.id.btnReset);
-        btnReset.getBackground().setAlpha(128);
-        btnReset.setVisibility(View.INVISIBLE);
-        btnReset.setOnClickListener(new OnClickListener(){
-			public void onClick(View arg0) {
-				mandelbrotView.Reset();
-			}
-        });
-        
+
         iterationsContainer = findViewById(R.id.iterationsContainer);
         iterationsContainer.setVisibility(View.GONE);
         seekBarIterations = (SeekBar)findViewById(R.id.seekBarIterations);
@@ -185,7 +138,28 @@ public class MandelbrotActivity extends Activity {
         seekBarIterations.setProgress((int)Math.sqrt(mandelbrotView.numIterations));
         
 	}
-	
+
+    /**
+     * Helper function to force the Activity to show the three-dot overflow icon in its ActionBar.
+     * @param activity Activity whose overflow icon will be forced.
+     */
+    private static void forceOverflowMenuIcon(Activity activity) {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(activity);
+            // Note: this field doesn't exist in 2.3, so those users will need to tap the physical menu button,
+            // unless we figure out another solution.
+            // This field also doesn't exist in 4.4, where the overflow icon is always shown:
+            // https://android.googlesource.com/platform/frameworks/base.git/+/ea04f3cfc6e245fb415fd352ed0048cd940a46fe%5E!/
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception ex) {
+            // multiple exceptions may be thrown above, but it's not super critical if it fails.
+        }
+    }
+
     @Override
     protected void onStop(){
     	super.onStop();
@@ -239,6 +213,55 @@ public class MandelbrotActivity extends Activity {
 	public boolean onTouchEvent(MotionEvent event){
 		return mandelbrotView.onTouchEvent(event);
 	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.menu_julia_mode:
+                mandelbrotView.juliaMode = !mandelbrotView.juliaMode;
+                mandelbrotView.RenderMandelbrot();
+                return true;
+            case R.id.menu_save_image:
+                try{
+                    String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+                    SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
+                    path += "/" + f.format(new Date()) + ".png";
+                    mandelbrotView.SavePicture(path);
+                    Toast.makeText(MandelbrotActivity.this, "Picture saved as: " + path, Toast.LENGTH_SHORT).show();
+                }catch(Exception ex){
+                    Toast.makeText(MandelbrotActivity.this, "Error saving file: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case R.id.menu_reset:
+                mandelbrotView.Reset();
+                return true;
+            case R.id.menu_about:
+                LayoutInflater inflater = (LayoutInflater)MandelbrotActivity.this.getSystemService(LAYOUT_INFLATER_SERVICE);
+                View layout = inflater.inflate(R.layout.about, null);
+                TextView txtAbout = (TextView)layout.findViewById(R.id.txtAbout);
+                txtAbout.setText(MandelbrotActivity.this.getString(R.string.str_about));
+                AlertDialog alertDialog = new AlertDialog.Builder(MandelbrotActivity.this).create();
+                alertDialog.setTitle("About...");
+                alertDialog.setView(layout);
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    } });
+                alertDialog.show();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
