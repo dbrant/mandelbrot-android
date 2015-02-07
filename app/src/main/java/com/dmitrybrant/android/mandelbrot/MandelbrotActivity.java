db2487909f77de065b910b533350e23b36eb3158
dbrant <dbrant@wikimedia.org>
2015-02-07 15:04:10 -0500

    A bit of a refactor.

--- app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotActivity.java
+++ app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotActivity.java
@@ -19,12 +19,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup.LayoutParams;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -32,21 +29,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MandelbrotActivity extends ActionBarActivity {
-	public static final String PREFS_NAME = "MandelbrotActivityPrefs";
+    public static final String PREFS_NAME = "MandelbrotActivityPrefs";
 
-	static {
-		System.loadLibrary("mandelnative_jni");
-	}
+    static {
+        System.loadLibrary("mandelnative_jni");
+    }
 
-	MandelbrotCanvas mandelbrotView;
-	FrameLayout topLayout;
-	View settingsContainer;
-	public TextView txtInfo, txtIterations;
-	SeekBar seekBarIterations;
+    private MandelbrotCanvas mandelbrotView;
+    private FrameLayout topLayout;
+    private View settingsContainer;
+    public TextView txtInfo, txtIterations;
+    private SeekBar seekBarIterations;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
         topLayout = (FrameLayout)findViewById(R.id.topLayout);
@@ -66,40 +63,39 @@ public class MandelbrotActivity extends ActionBarActivity {
 
         seekBarIterations = (SeekBar)findViewById(R.id.seekBarIterations);
         seekBarIterations.setMax((int)Math.sqrt(MandelbrotCanvas.MAX_ITERATIONS) + 1);
-        seekBarIterations.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-				if(!fromUser)
-					return;
-				mandelbrotView.numIterations = progress * progress;
-				if(mandelbrotView.numIterations < MandelbrotCanvas.MIN_ITERATIONS)
-					mandelbrotView.numIterations = MandelbrotCanvas.MIN_ITERATIONS;
-				if(mandelbrotView.numIterations > MandelbrotCanvas.MAX_ITERATIONS)
-					mandelbrotView.numIterations = MandelbrotCanvas.MAX_ITERATIONS;
-				mandelbrotView.RenderMandelbrot();
-			}
-			public void onStartTrackingTouch(SeekBar arg0) {
-			}
-			public void onStopTrackingTouch(SeekBar arg0) {
-			}
+        seekBarIterations.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (!fromUser) {
+                    return;
+                }
+                mandelbrotView.numIterations = progress * progress;
+                if (mandelbrotView.numIterations < MandelbrotCanvas.MIN_ITERATIONS) {
+                    mandelbrotView.numIterations = MandelbrotCanvas.MIN_ITERATIONS;
+                }
+                if (mandelbrotView.numIterations > MandelbrotCanvas.MAX_ITERATIONS) {
+                    mandelbrotView.numIterations = MandelbrotCanvas.MAX_ITERATIONS;
+                }
+                mandelbrotView.RenderMandelbrot();
+            }
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+            public void onStopTrackingTouch(SeekBar arg0) {
+            }
         });
         
         mandelbrotView = new MandelbrotCanvas(this);
         topLayout.addView(mandelbrotView, 0, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         
         //restore settings...
-        try{
-        	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        	mandelbrotView.xcenter = Double.parseDouble(settings.getString("xcenter", "-0.5"));
-        	mandelbrotView.ycenter = Double.parseDouble(settings.getString("ycenter", "0.0"));
-        	mandelbrotView.xextent = Double.parseDouble(settings.getString("xextent", "3.0"));
-        	mandelbrotView.numIterations = settings.getInt("iterations", 128);
-        	mandelbrotView.currentColorScheme = settings.getInt("colorscheme", 0);
-        	
-        }catch(Exception ex){ }
-        
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        mandelbrotView.xcenter = Double.parseDouble(settings.getString("xcenter", "-0.5"));
+        mandelbrotView.ycenter = Double.parseDouble(settings.getString("ycenter", "0.0"));
+        mandelbrotView.xextent = Double.parseDouble(settings.getString("xextent", "3.0"));
+        mandelbrotView.numIterations = settings.getInt("iterations", 128);
+        mandelbrotView.currentColorScheme = settings.getInt("colorscheme", 0);
+
         seekBarIterations.setProgress((int)Math.sqrt(mandelbrotView.numIterations));
-        
-	}
+    }
 
     /**
      * Helper function to force the Activity to show the three-dot overflow icon in its ActionBar.
@@ -108,7 +104,7 @@ public class MandelbrotActivity extends ActionBarActivity {
     private static void forceOverflowMenuIcon(Activity activity) {
         try {
             ViewConfiguration config = ViewConfiguration.get(activity);
-            // Note: this field doesn't exist in 2.3, so those users will need to tap the physical menu button,
+            // Note: this field doesn't exist in API <11, so those users will need to tap the physical menu button,
             // unless we figure out another solution.
             // This field also doesn't exist in 4.4, where the overflow icon is always shown:
             // https://android.googlesource.com/platform/frameworks/base.git/+/ea04f3cfc6e245fb415fd352ed0048cd940a46fe%5E!/
@@ -124,57 +120,56 @@ public class MandelbrotActivity extends ActionBarActivity {
 
     @Override
     protected void onStop(){
-    	super.onStop();
-    	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-    	SharedPreferences.Editor editor = settings.edit();
-    	editor.putString("xcenter", Double.toString(mandelbrotView.xcenter));
-    	editor.putString("ycenter", Double.toString(mandelbrotView.ycenter));
-    	editor.putString("xextent", Double.toString(mandelbrotView.xextent));
-    	editor.putInt("iterations", mandelbrotView.numIterations);
-    	editor.putInt("colorscheme", mandelbrotView.currentColorScheme);
-    	editor.commit();
+        super.onStop();
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("xcenter", Double.toString(mandelbrotView.xcenter));
+        editor.putString("ycenter", Double.toString(mandelbrotView.ycenter));
+        editor.putString("xextent", Double.toString(mandelbrotView.xextent));
+        editor.putInt("iterations", mandelbrotView.numIterations);
+        editor.putInt("colorscheme", mandelbrotView.currentColorScheme);
+        editor.commit();
     }
     
-	@Override
-	public void onDestroy(){
-		mandelbrotView.TerminateThread();
-		
-		mandelnative.ReleaseParameters();
-		mandelnative.ReleaseBitmap();
-		super.onDestroy();
-	}
-	
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		int iterationInc = mandelbrotView.numIterations / 16;
-		if(iterationInc < 1)
-			iterationInc = 1;
-		if (keyCode == KeyEvent.KEYCODE_BACK){
-			finish();
-			return true;
-		}
-		else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
-			mandelbrotView.numIterations -= iterationInc;
-			if(mandelbrotView.numIterations < MandelbrotCanvas.MIN_ITERATIONS)
-				mandelbrotView.numIterations = MandelbrotCanvas.MIN_ITERATIONS;
-			seekBarIterations.setProgress((int)Math.sqrt(mandelbrotView.numIterations));
-			mandelbrotView.RenderMandelbrot();
-			return true;
-		}
-		else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP){
-			mandelbrotView.numIterations += iterationInc;
-			if(mandelbrotView.numIterations > MandelbrotCanvas.MAX_ITERATIONS)
-				mandelbrotView.numIterations = MandelbrotCanvas.MAX_ITERATIONS;
-			seekBarIterations.setProgress((int)Math.sqrt(mandelbrotView.numIterations));
-			mandelbrotView.RenderMandelbrot();
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-	
-	public boolean onTouchEvent(MotionEvent event){
-		return mandelbrotView.onTouchEvent(event);
-	}
+    @Override
+    public void onDestroy(){
+        mandelbrotView.TerminateThread();
+
+        mandelnative.ReleaseParameters();
+        mandelnative.ReleaseBitmap();
+        super.onDestroy();
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        int iterationInc = mandelbrotView.numIterations / 16;
+        if (iterationInc < 1) {
+            iterationInc = 1;
+        }
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            finish();
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+            mandelbrotView.numIterations -= iterationInc;
+            if(mandelbrotView.numIterations < MandelbrotCanvas.MIN_ITERATIONS)
+                mandelbrotView.numIterations = MandelbrotCanvas.MIN_ITERATIONS;
+            seekBarIterations.setProgress((int)Math.sqrt(mandelbrotView.numIterations));
+            mandelbrotView.RenderMandelbrot();
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+            mandelbrotView.numIterations += iterationInc;
+            if(mandelbrotView.numIterations > MandelbrotCanvas.MAX_ITERATIONS)
+                mandelbrotView.numIterations = MandelbrotCanvas.MAX_ITERATIONS;
+            seekBarIterations.setProgress((int)Math.sqrt(mandelbrotView.numIterations));
+            mandelbrotView.RenderMandelbrot();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    
+    public boolean onTouchEvent(MotionEvent event){
+        return mandelbrotView.onTouchEvent(event);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -228,7 +223,8 @@ public class MandelbrotActivity extends ActionBarActivity {
                 alertDialog.setView(layout);
                 alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                    } });
+                    }
+                });
                 alertDialog.show();
                 return true;
             default:
@@ -238,5 +234,3 @@ public class MandelbrotActivity extends ActionBarActivity {
     }
 
 }
-
-
