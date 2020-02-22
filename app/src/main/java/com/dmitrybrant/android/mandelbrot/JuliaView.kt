c3c7d219c7e20380bb8393c32937b6a6d48ea0bd
Dmitry Brant <dbrant@wikimedia.org>
2020-02-22 15:26:53 -0500

    Convert views to Kotlin.

---
+++ app/src/main/java/com/dmitrybrant/android/mandelbrot/JuliaView.kt
@@ -0,0 +1,19 @@
+package com.dmitrybrant.android.mandelbrot
+
+import android.content.Context
+import android.util.AttributeSet
+
+class JuliaView : MandelbrotViewBase {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        setup()
+    }
+
+    private fun setup() {
+        if (isInEditMode) {
+            return
+        }
+        super.setup(true)
+    }
+}
