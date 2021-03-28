7c2ef5800dd02dc0d0def64f038dd95cae1f0bab
Dmitry Brant <dbrant@wikimedia.org>
2021-03-28 13:30:19 -0400

    Additional cleanup.

--- app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotView.kt
+++ app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotView.kt
@@ -3,17 +3,11 @@ package com.dmitrybrant.android.mandelbrot
 import android.content.Context
 import android.util.AttributeSet
 
-class MandelbrotView : MandelbrotViewBase {
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        setup()
-    }
-
-    private fun setup() {
-        if (isInEditMode) {
-            return
+class MandelbrotView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        MandelbrotViewBase(context, attrs, defStyleAttr) {
+    init {
+        if (!isInEditMode) {
+            super.setup(false)
         }
-        super.setup(false)
     }
 }
