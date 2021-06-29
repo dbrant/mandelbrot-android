9a3458dd5d16b6eca33f6531d2773dc049d82997
Dmitry Brant <dbrant@wikimedia.org>
2021-06-29 10:39:53 -0400

    Optimize some resources.

--- app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotActivity.java
+++ app/src/main/java/com/dmitrybrant/android/mandelbrot/MandelbrotActivity.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.Gravity;
@@ -52,9 +53,11 @@ public class MandelbrotActivity extends Activity {
 
         ColorScheme.initColorSchemes();
 
-        if (getActionBar() != null) {
-            getActionBar().setDisplayHomeAsUpEnabled(true);
-            getActionBar().setTitle("");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            if (getActionBar() != null) {
+                getActionBar().setDisplayHomeAsUpEnabled(true);
+                getActionBar().setTitle("");
+            }
         }
 
         txtInfo = findViewById(R.id.txtInfo);
